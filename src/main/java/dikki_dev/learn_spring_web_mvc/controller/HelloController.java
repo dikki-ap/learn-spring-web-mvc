e8385fac32fd32d532f78fd7f2ec1c3e6b7b1c72
Dikki AP <dikki.1710@gmail.com>
2025-11-03 22:22:05 +0700

    1. '@Controller' Annotation

---
+++ src/main/java/dikki_dev/learn_spring_web_mvc/controller/HelloController.java
@@ -0,0 +1,7 @@
+package dikki_dev.learn_spring_web_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller // Tambahkan "@Controller" Annotation agar menandai Class ini menjadi Controller (otomatis sudah menjadi BEAN / Component)
+public class HelloController {
+}
