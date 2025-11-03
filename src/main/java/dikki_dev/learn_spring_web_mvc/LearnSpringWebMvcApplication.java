65fda34a3f414e15c1a15f0a74fe8aead09684ea
Dikki AP <dikki.1710@gmail.com>
2025-11-03 22:19:09 +0700

    Initial

---
+++ src/main/java/dikki_dev/learn_spring_web_mvc/LearnSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_web_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dikki_dev/learn_spring_web_mvc/LearnSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
