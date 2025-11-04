d304179e282f74f8c9056897dd2f5e59f4ac77d2
Dikki AP <dikki.agung@epson.co.id>
2025-11-04 13:51:50 +0700

    7. '@MockBean' Annotation

--- src/main/java/dikki_dev/learn_spring_web_mvc/controller/HelloController.java
+++ src/main/java/dikki_dev/learn_spring_web_mvc/controller/HelloController.java
@@ -1,7 +1,9 @@
 package dikki_dev.learn_spring_web_mvc.controller;
 
+import dikki_dev.learn_spring_web_mvc.services.HelloService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,15 +13,17 @@ import java.util.Objects;
 @Controller // Tambahkan "@Controller" Annotation agar menandai Class ini menjadi Controller (otomatis sudah menjadi BEAN / Component)
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     // Akses "http://localhost:yourPort/hello" untuk tes ini
     // Akses http://localhost:9090/hello?name="Dikki" untuk test ini dengan request parameter
     @RequestMapping(path = "/hello") // Tambahkan "@RequestMapping" Annotation untuk menjadikan method sebagai Endpoint dengan "path" paramnya
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException{
         String name = request.getParameter("name");
 
-        if(Objects.isNull(name)){
-            name = "Guest";
-        }
-        response.getWriter().println("Hello " + name);
+        String responseBody = helloService.hello(name);
+
+        response.getWriter().println(responseBody);
     }
 }
--- src/test/java/dikki_dev/learn_spring_web_mvc/controller/HelloControllerTest.java
+++ src/test/java/dikki_dev/learn_spring_web_mvc/controller/HelloControllerTest.java
@@ -1,10 +1,15 @@
 package dikki_dev.learn_spring_web_mvc.controller;
 
+import dikki_dev.learn_spring_web_mvc.services.HelloService;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +22,22 @@ public class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // Menggunakan @MockBean untuk membuat "Mock" dari Hello Service
+    // Kita bisa mengubah Behaviour nya juga
+    @MockBean
+    private HelloService helloService;
+
+    @BeforeEach
+    void setUp(){
+        // Mengubah Behaviour dari helloService, jika string apapun yang masuk maka jadi "Hello Guys"
+        Mockito.when(helloService.hello(null))
+                .thenReturn("Hello Guest");
+
+        // Mengubah Behaviour dari helloService, jika string apapun yang masuk maka jadi "Hello Guys"
+        Mockito.when(helloService.hello(Mockito.anyString()))
+                .thenReturn("Hello Guys");
+    }
 
     @Test
     void helloGuest() throws Exception {
@@ -36,7 +57,7 @@ public class HelloControllerTest {
                 .andDo(print()) // Menampilkan hasil response di console saat test berjalan
                 .andExpectAll(
                         status().isOk(),
-                        content().string(Matchers.containsString("Hello Dikki"))
+                        content().string(Matchers.containsString("Hello Guys"))
                 );
     }
 }
